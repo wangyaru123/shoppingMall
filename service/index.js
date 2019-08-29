9661dc7c698f4bf662be8e77e08bffcb6ad4a0d3
wangyaru <2420576216@qq.com>
2019-08-29 09:47:42 +0800

    feat(project): 1# 服务端router,post处理，跨域处理，安装koa2-cors; 2# 注册页面

--- service/index.js
+++ service/index.js
@@ -2,19 +2,39 @@ const Koa = require('koa')
 const app = new Koa()
 const { connect, initSchemas } = require('./database/init.js')
 const mongoose = require('mongoose')
+const bodyParser = require('koa-bodyparser')
+const cors = require('koa2-cors')
 
-  //立即执行函数
-  ; (async () => {
-    await connect()
-    initSchemas()
-    const User = mongoose.model('User')
-    let oneUser = new User({ userName: 'jspan2g', password: '123456' })
-    oneUser.save().then(() => {
-      console.log('插入成功')
-    })
-    let users = await User.findOne({}).exec()
-    console.log(users)
-  })()
+const Router = require('koa-router')
+
+// 若放在后面会出问题
+app.use(bodyParser());
+app.use(cors())
+
+let user = require('./api/user.js')
+let home = require('./api/home.js')
+
+// 装载所有子路由
+let router = new Router();
+router.use('/user', user.routes())
+router.use('/home', home.routes())
+
+// 加载路由中间件
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+//立即执行函数
+// ; (async () => {
+//   await connect()
+//   initSchemas()
+//   const User = mongoose.model('User')
+//   let oneUser = new User({ userName: 'jspan2g', password: '123456' })
+//   oneUser.save().then(() => {
+//     console.log('插入成功')
+//   })
+//   let users = await User.findOne({}).exec()
+//   console.log(users)
+// })()
 
 app.use(async (ctx) => {
   ctx.body = '<h1>hello Koa2</h1>'
--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,8 +1,11 @@
 const BASEURL = 'https://www.easy-mock.com/mock/5d639972f33d9b4fd23044d0/smiledemo'
+const LOCALURL = 'http://localhost:3000'
 
 const URL = {
-  getShoppingMallInfo: BASEURL + 'index',
-  getGoodsInfo: BASEURL + 'getGoodsInfo'
+  getShoppingMallInfo: BASEURL + '/index',
+  getGoodsInfo: BASEURL + '/getGoodsInfo',
+  //用户注册接口
+  registerUser: LOCALURL + '/user/register'
 }
 
 module.exports = URL
