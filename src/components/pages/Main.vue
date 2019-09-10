ed24ebb05966f34e4bbd11e97f0835e62069af93
wangyaru <2420576216@qq.com>
2019-09-10 10:17:30 +0800

    feat(pages): 1# tabbar; 2# 用户中心

---
+++ src/components/pages/Main.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="main-div">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
+    <van-tabbar v-model="active" @change="changeTabbar(active)">
+      <van-tabbar-item icon="shop">首页</van-tabbar-item>
+      <van-tabbar-item icon="records">列表</van-tabbar-item>
+      <van-tabbar-item icon="cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact">会员中心</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      nowPath: '',  //当前路径
+    }
+  },
+  created() {
+    this.changeTabBarActive()
+  },
+  updated() {
+    this.changeTabBarActive()
+  },
+  methods: {
+    // 购物车页面出现底部导航不跟随变化的主要问题是，我们不是点击导航进入的，而是通过编程式导航进入的，这样导航栏就没有互动和触发事件，所以并没有发生变化
+    changeTabBarActive() {
+      this.nowPath = this.$route.path
+      if (this.nowPath == '/Cart') {
+        this.active = 2
+      }
+    },
+    changeTabbar(active) {
+      console.log(active)
+      switch (active) {
+        case 0:
+          this.$router.push({ name: 'ShoppingMall' })
+          break;
+        case 1:
+          this.$router.push({ name: 'CategoryList' })
+          break;
+        case 2:
+          this.$router.push({ name: 'Cart' })
+          break;
+        case 3:
+          this.$router.push({ name: 'User' })
+          break;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/modules/mobile.js
+++ src/router/modules/mobile.js
@@ -5,39 +5,23 @@ import Login from '@/components/pages/Login'
 import Goods from '@/components/pages/Goods'
 import CategoryList from '@/components/pages/CategoryList'
 import Cart from '@/components/pages/Cart'
+import Main from '@/components/pages/Main'
+import User from '@/components/pages/User'
 
 // 报警
 const mobileRouter = [
   {
-    path: '/',
-    name: 'ShoppingMall',
-    component: ShoppingMall
+    path: '/main', name: 'Main', component: Main,
+    children: [
+      { path: '/', name: 'ShoppingMall', component: ShoppingMall },
+      { path: '/CategoryList', name: 'CategoryList', component: CategoryList },
+      { path: '/Cart', name: 'Cart', component: Cart },
+      { path: '/User', name: 'User', component: User }
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/Goods',
-    name: 'Goods',
-    component: Goods
-  },
-  {
-    path: '/CategoryList',
-    name: 'CategoryList',
-    component: CategoryList
-  },
-  {
-    path: '/Cart',
-    name: 'Cart',
-    component: Cart
-  }
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/Goods', name: 'Goods', component: Goods }
 ]
 
 export default mobileRouter
